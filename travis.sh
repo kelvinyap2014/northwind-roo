c0650e1cd2755b66b68bf78b6d68243fed50e9e7
kelvinyap2014 <kelvinyap1998@gmail.com>
2017-04-25 21:36:36 +1200

    Fix master build failure due to Sonar configurations.
    Issue #26

--- travis.sh
+++ travis.sh
@@ -5,17 +5,13 @@ cd app
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ] 
 then
     echo "Running build and Sonar analysis for Pull Request '${TRAVIS_PULL_REQUEST}'"
-    ./mvnw clean test sonar:sonar \
-        -Dsonar.host.url=https:/sonarqube.com \
-        -Dsonar.login=${SONARQUBE_TOKEN} \
-        -Dsonar.organization=kelvinyap2014-github \
-		-Dsonar.analysis.mode=preview
+    ./mvnw clean test sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.analysis.mode=preview
 elif [ "${TRAVIS_BRANCH}" = "master" ]
 then
     echo "Running build and Sonar analysis for ${TRAVIS_BRANCH} branch"
     ./mvnw clean test sonar:sonar \
-        -Dsonar.host.url=https:/sonarqube.com \
-        -Dsonar.login=${SONARQUBE_TOKEN} \
+        -Dsonar.host.url=https://sonarqube.com \
+        -Dsonar.login=${SONAR_TOKEN} \
         -Dsonar.organization=kelvinyap2014-github
 else
     echo "Running build for ${TRAVIS_BRANCH} branch"
